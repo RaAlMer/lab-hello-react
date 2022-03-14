8eb4028e85120c9a72a1c4f8206060b98cf705b0
Raúl <raul.alonso.merino@gmail.com>
2022-03-14 14:54:31 +0100

    done

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import ironHackLogo from './images/ironhack-logo-xs.png';
+import menuTop from './images/menu-top-xs.png';
+import icon1 from './images/icon1.png';
+import icon2 from './images/icon2.png';
+import icon3 from './images/icon3.png';
+import icon4 from './images/icon4.png';
 
 function App() {
   return (
     <div className="App">
       <div className="content">
         <div className="images">
-          <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png" alt="logo" />
-          <img className="menu" src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png" alt="menu"/>
+          <img src={ironHackLogo} alt="logo" />
+          <img className="menu" src={menuTop} alt="menu"/>
         </div>
         <div className="info">
           <h1>Say hello to<br/>ReactJS</h1>
@@ -16,22 +22,22 @@ function App() {
       </div>
       <div className="icons">
         <div>
-          <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png" alt="icon1" />
+          <img src={icon1} alt="icon1" />
           <h2>Declarative</h2>
           <p>React makes it<br/>painless to create<br/>interactive UIs.</p>
         </div>
         <div>
-        <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png" alt="icon2" />
+        <img src={icon2} alt="icon2" />
           <h2>Components</h2>
           <p>Build encapsulated<br/>components that<br/>manage their state.</p>
         </div>
         <div>
-        <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png" alt="icon3" />
+        <img src={icon3} alt="icon3" />
           <h2>Single-Way</h2>
           <p>A set of immutable<br/>values are passed to<br/>the component's.</p>
         </div>
         <div>
-        <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png" alt="icon4" />
+        <img src={icon4} alt="icon4" />
           <h2>JSX</h2>
           <p>Statically-typed,<br/>designed to run on<br/>modern browsers</p>
         </div>
